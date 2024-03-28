4cc5e37747ac49984f72ff401611e832a18e58ea
biohackerellie <epkerns@gmail.com>
2024-03-28 17:16:49 -0600

    Update investment amount and years variables in investment_calculator.go

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,10 @@ import (
 
 
 func main() {
-	investmentAmount, years := 1000.0, 10.0
+	var investmentAmount float64 = 1000
+	years:= 10.0
 	expectedReturnRate := 5.5
+
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
 
 	fmt.Println(futureValue)
