fb1661f56e92e5f06879bb22e22aec5001c8f1c6
biohackerellie <epkerns@gmail.com>
2024-03-28 17:39:50 -0600

    Add user input for investment amount, number of years, and expected return rate

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,11 +7,23 @@ import (
 
 
 func main() {
-	var investmentAmount float64 = 1000
-	years:= 10.0
-	expectedReturnRate := 5.5
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years) 
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
